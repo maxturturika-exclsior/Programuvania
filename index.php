fb427b0dfa4eed5cf11eb571d204146473f2323e
max-exclsior <max.turturika@gmail.com>
2026-09-18 21:38:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
-    echo "<h1>Hello World! Це моя перша PHP-програма.</h1>";
-    phpinfo();
+$appName ="My App";
+$taskTitle = "Learn PHP operators";
+$taskTimeEstimate = "1 hour";
 ?>
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1> <?= $appName ?></h1>
+        <ul>
+            <li><?=$taskTitle?></li>
+            <li><?=$taskTimeEstimate?></li>
+        </ul>
+    </header>
+    <main>
+
+
+    </main>    
+</body>
+</html>
