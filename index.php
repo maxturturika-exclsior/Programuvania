3ccac8b8fb51ce70245f28a56945ba11d4cb6aa0
max-exclsior <max.turturika@gmail.com>
2026-09-19 15:52:23 +0300

    Updated PHP task

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+$result;
+$isCompleted = false;
 $appName ="My App";
 $taskTitle = "Learn PHP operators";
 $taskTimeEstimate = "1 hour";
+
+$result = ($isCompleted == true) ? "Виконано!" : "В процесі";
 ?>
 <!DOCTYPE html>
 <html lang = "en">
@@ -9,12 +13,25 @@ $taskTimeEstimate = "1 hour";
     <meta charset = "UTF-8">
     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        .task-done {
+            color: green;
+            text-decoration: line-through;
+        }
+
+        .task-pending {
+            color: gray;
+        }
+    </style>
 </head>
 <body>
     <header>
         <h1> <?= $appName ?></h1>
         <ul>
-            <li><?=$taskTitle?></li>
+            <li class="<?= $isCompleted == true ? 'task-done' : 'task-pending' ?>">
+                <?= $result ?> <?= $taskTitle ?>
+            </li>
             <li><?=$taskTimeEstimate?></li>
         </ul>
     </header>
