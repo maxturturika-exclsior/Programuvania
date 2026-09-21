513b8fb7b02e03f670026b1c32cfc8a34567068f
max-exclsior <max.turturika@gmail.com>
2026-09-21 12:19:18 +0300

    Fix PHP functions

--- index.php
+++ index.php
@@ -2,8 +2,23 @@
 $result;
 $isCompleted = false;
 $appName ="My App";
-$taskTitle = "Learn PHP operators";
+$taskTitle = "Learn PHP basics - operators, variables, functions and string";
 $taskTimeEstimate = "1 hour";
+function formatTitle($text, $maxLenght = 20) {
+    if ((strlen($text)) >= $maxLenght)
+        return substr($text, 0, $maxLenght) . "...";
+    else
+        return $text;
+}
+
+function getCurrentGreeting($Time){
+    if ((date($Time)) >= "06:00" && (date($Time)) <= "11:59")
+        echo "Доброго ранку";
+    elseif ((date($Time)) >= "12:00" && (date($Time)) <= "17:59")
+        echo "Добрий день";
+     elseif ((date($Time)) >= "18:00" && (date($Time)) <= "23:59")
+        echo "Добрий вечір";
+}
 
 $result = ($isCompleted == true) ? "Виконано!" : "В процесі";
 ?>
@@ -27,10 +42,10 @@ $result = ($isCompleted == true) ? "Виконано!" : "В процесі";
 </head>
 <body>
     <header>
-        <h1> <?= $appName ?></h1>
+        <h1> <?= getCurrentGreeting(date('H:i:s')) ?> </h1>
         <ul>
             <li class="<?= $isCompleted == true ? 'task-done' : 'task-pending' ?>">
-                <?= $result ?> <?= $taskTitle ?>
+                <?= $result ?> <?= formatTitle($taskTitle) ?>
             </li>
             <li><?=$taskTimeEstimate?></li>
         </ul>
